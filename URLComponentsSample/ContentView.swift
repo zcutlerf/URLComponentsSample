da8dd4dddde6a543c3a50f3aab689e423f2d7363
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2024-02-06 16:25:09 -0500

    Initial Commit

---
+++ URLComponentsSample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLComponentsSample
+//
+//  Created by Zoe Cutler on 2/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLComponentsSample/URLComponentsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLComponentsSampleApp.swift
+//  URLComponentsSample
+//
+//  Created by Zoe Cutler on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct URLComponentsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
